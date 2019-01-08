2169f07ed2f3bb6acfc0a36f6f930b1c752cb047
Isaac Alfaro <sac.alf@gmail.com>
2019-01-08 15:30:38 -0600

    Add project files.

---
+++ EventsApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventsApp.Startup))]
+namespace EventsApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
