7686b29e04ba4394dd95a0068911d1d5700877b3
Isaac Alfaro <sac.alf@gmail.com>
2019-01-11 00:21:04 -0600

    Add name to artist

--- EventsApp/Controllers/AccountController.cs
+++ EventsApp/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using EventsApp.Models;
+using EventsApp.ViewModels;
 
 namespace EventsApp.Controllers
 {
@@ -151,7 +152,12 @@ namespace EventsApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- EventsApp/Controllers/HomeController.cs
+++ EventsApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EventsApp.Models;
+using System.Data.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,21 @@ namespace EventsApp.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Where(g => g.DateTime > DateTime.Now);
+
+            return View(upcomingGigs);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
