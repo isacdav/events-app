aae0a4e371e170d97d9b1838a40a696bc3045a1a
Isaac Alfaro <sac.alf@gmail.com>
2019-01-10 21:30:25 -0600

    Implement Validation

--- EventsApp/Controllers/GigsController.cs
+++ EventsApp/Controllers/GigsController.cs
@@ -30,10 +30,17 @@ namespace EventsApp.Controllers
         [HttpPost]
         public ActionResult Create(GigFormViewModel viewModel)
         {
+
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View(viewModel);
+            }
+
             var gig = new Gig
             {
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = viewModel.DateTime,
+                DateTime = viewModel.GetDateTime(),
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
             };
