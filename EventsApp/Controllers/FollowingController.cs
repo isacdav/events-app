2637682295548c4f9d73fae3691f7a107e91b1b8
Isaac Alfaro <sac.alf@gmail.com>
2019-01-12 23:17:12 -0600

    Implementing follow

---
+++ EventsApp/Controllers/FollowingController.cs
@@ -0,0 +1,48 @@
+﻿using EventsApp.DTOs;
+using EventsApp.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EventsApp.Controllers
+{
+    [Authorize]
+    public class FollowingController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FollowingController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+            var exists = _context.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == dto.FolloweeId);
+
+            if (exists)
+            {
+                return BadRequest("Following already exists");
+            }
+
+            var following = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = dto.FolloweeId
+            };
+
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+
+        }
+
+    }
+}
